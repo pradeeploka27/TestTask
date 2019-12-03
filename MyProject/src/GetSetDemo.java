d65acd8a56977baa601d978b1c79a521c225ff30
Lab-1 <Lab-1@DESKTOP-UHM8TAB>
2019-12-03 14:41:13 +0530

    This is My First Project

---
+++ MyProject/src/GetSetDemo.java
@@ -0,0 +1,19 @@
+
+public class GetSetDemo {
+	String MyBankName;
+	int MyBankAccountNumber;
+	public String getMyBankName() {
+		return MyBankName;
+	}
+	public void setMyBankName(String myBankName) {
+		MyBankName = myBankName;
+	}
+	public int getMyBankAccountNumber() {
+		return MyBankAccountNumber;
+	}
+	public void setMyBankAccountNumber(int myBankAccountNumber) {
+		MyBankAccountNumber = myBankAccountNumber;
+	}
+	
+
+}
